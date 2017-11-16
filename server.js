b87ebcba3402c3fca6db781a6fa4d8bb0eac8d96
Jeremy Noh <jeremynoh@MacBook-Pro-de-Wilson.local>
2017-11-16 14:43:50 +0100

    ajout des routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express      = require('express');
 const app  = express();
 const path         = require('path');
-
+const sequelize = require('sequelize');
 const port = process.argv[2] || 8080;
 
 
@@ -10,21 +10,83 @@ const port = process.argv[2] || 8080;
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
+// connexion a la base
+
+const
+
 // midlleWare
 app.use(express.static(path.join(__dirname,'public')));
 
 
 
+// accède a la page index si url : "/" ou "index"
 app.get('/', function(request, response) {
     response.render('index');
 
 });
-
 app.get(/^\/(index)?$/, function(request, response) {
     response.render('index');
 });
 
+// sequelize
 
+
+// accède a la fonctionnalité sign-in
+app.get('/sign-in', function(request, response) {
+    response.render('sign-in');
+});
+
+app.post('/sign-in', function(request, response) {
+    response.render('sign-in');
+});
+
+
+// accède a la fonctionnalité sign-up
+app.get('/sign-up', function(request, response) {
+    response.render('sign-up');
+});
+app.post('/sign-up', function(request, response) {
+    response.render('sign-up');
+});
+
+// accède a la fonctionnalité profile
+app.get('/profile/:id', function(request, response) {
+    response.render('profile');
+});
+
+app.get('/profile/:id/edit', function(request, response) {
+    response.render('edit');
+});
+
+app.put('/profile:id', function(request, response) {
+    response.render('edit');
+});
+app.delete('/profile:id', function(request, response) {
+    response.render('edit');
+});
+
+app.get('/profile:id/modules', function(request, response) {
+    response.render('edit');
+});
+
+app.post('/profile:id/modules', function(request, response) {
+    response.render('edit');
+});
+
+app.delete('/profile:id/modules', function(request, response) {
+    response.render('edit');
+});
+app.put('/profile:id/modules', function(request, response) {
+    response.render('edit');
+});
+
+
+
+
+
+sequelize.authenticate().then(()=>{
+
+})
 // portListen
 app.listen(port,()=>{
   console.log(`Server is running on port : ${port} `);
