4bd2b443f313203ffbfc5e34a18b7a8bbc76c228
Jeremy Noh <jeremynoh@MacBook-Pro-de-Wilson.local>
2017-11-17 17:09:03 +0100

    fonctinnalité dashboard OK

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router  = express.Router();
+const citation = require('request');
+const moment = require('moment');
+const bodyParser = require('body-parser');
 
 const db = require('../database/init');
 
@@ -15,12 +18,22 @@ router.get('/:name', (request, response) => {
 
 // accède a la fonctionnalité profile
 router.get('/profile/:id', function (request, response) {
-    response.render('profile');
+  response.render('profile');
 });
 
 router.get('/dashboard/:id', function (request, response) {
-    response.render('dashboard');
-});
+
+  let InfoDate = moment().format('LL');
+  let id = request.params.id
+  let url = 'http://quotes.stormconsultancy.co.uk/random.json'
+  citation (url,(error,res,body)=>{
+    console.log(body.permalink);
+
+    let user = db.user.findOne({where: {id :request.params.id}}).then(user=>{
+      response.render('dashboard',{user,InfoDate,body :JSON.parse(body)});
+    })
+  });
+  });
 
 
 
